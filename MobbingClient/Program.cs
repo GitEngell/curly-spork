7bdc1fb28cc20795fa144d456cdb1958c309b020
Claus Engell Berthelsen <clau795b@edu.easj.dk>
2016-10-14 11:24:45 +0200

    Secondtry

---
+++ MobbingClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Nito.AsyncEx;
+
+namespace MobbingClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncContext.Run(() => MainAsync(args));
+        }
+
+        static async void MainAsync(string[] args)
+        {
+            var page = "http://localhost:9192/MyRestService.svc/GetProductList/";
+            var client = new HttpClient();
+            var response = await client.GetAsync(page);
+            var content = response.Content;
+            var result = content.ReadAsStringAsync();
+
+            Console.WriteLine(result.Result);
+
+            Console.WriteLine("Finished");
+            Console.ReadLine();
+        }
+    }
+}
